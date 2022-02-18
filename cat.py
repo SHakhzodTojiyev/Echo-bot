5f3c6cc2017b813b74e463dc421c71032a7875db
Shakhzod Tojiyev <68785236+SHakhzodTojiyev@users.noreply.github.com>
2022-02-18 13:41:49 +0500

    New file here

---
+++ cat.py
@@ -0,0 +1,25 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'Qovuncha'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    photo = "https://i.ibb.co/8mqGrMr/image.png"
+    await message.reply_photo(photo, caption='Cats are here 😺')
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
